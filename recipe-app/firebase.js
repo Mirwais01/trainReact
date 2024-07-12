ccd950a2b18ab88fb1d03a373fd521dff97b9764
Mirwais <mirwais.sanaie@gmail.com>
2024-07-12 22:22:08 +0430

    hosted fixed

---
+++ recipe-app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2ZkSElE829IMTHyFv016HY3xPpZGlsDQ",
+  authDomain: "recipe-app-aade5.firebaseapp.com",
+  projectId: "recipe-app-aade5",
+  storageBucket: "recipe-app-aade5.appspot.com",
+  messagingSenderId: "871578990045",
+  appId: "1:871578990045:web:a0545b2e5a37adeba97234"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
